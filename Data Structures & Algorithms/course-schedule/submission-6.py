a6a5652d62eca7d0d64355965f0bd5b8e87914ca
taranroyyuru <38592982+taranroyyuru@users.noreply.github.com>
2026-09-18 12:37:07 -0700

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hashmap = defaultdict(list)
+        visit = set()
+
+        for crs, pre in prerequisites:
+            hashmap[crs].append(pre)
+
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if not hashmap[crs]:
+                return True
+            visit.add(crs)
+            for pre in hashmap[crs]:
+                if not dfs(pre):
+                    return False
+            visit.remove(crs)
+            hashmap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+
+        
+                    
+
+
+
+            
+        
+        
